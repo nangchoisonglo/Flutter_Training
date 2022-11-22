0da4eb0295089ac69aff24ec5c3cd04b8bac55b9
nangchoisonglo <118276246+nangchoisonglo@users.noreply.github.com>
2022-11-22 18:48:50 +0700

    1

---
+++ first_lession/bin/first_lession.dart
@@ -0,0 +1,5 @@
+import 'package:first_lession/first_lession.dart' as first_lession;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_lession.calculate()}!');
+}
---
+++ first_lession/test/first_lession_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_lession/first_lession.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
