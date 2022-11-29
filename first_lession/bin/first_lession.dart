3a012260a35e2bf9394a800d8e37b747e47d9023
nangchoisonglo <118276246+nangchoisonglo@users.noreply.github.com>
2022-11-29 18:54:41 +0700

    Update first_lession.dart

--- first_lession/bin/first_lession.dart
+++ first_lession/bin/first_lession.dart
@@ -1,5 +1,21 @@
-import 'package:first_lession/first_lession.dart' as first_lession;
+void main(List<String> args) {
+  String str = "Hello World";
+  int a = 10;
 
-void main(List<String> arguments) {
-  print('Hello world: ${first_lession.calculate()}!'); abcd
+  print(str == a ||
+      a ==
+          10); // `||` là hoặc, 1 trong 2 điều kiện đúng -> true; sẽ in ra: false
+  print(str == a &&
+      a == 10); // `&&` là hoặc, cả 2 điều kiện đúng -> true; sẽ in ra: false
+
+  /// 3 cách viết này cho ra kết quả giống nhau:
+  if ((str == a) == false) {
+    print("true");
+  }
+  if (!(str == a)) {
+    print("true");
+  }
+  if (str != a) {
+    print("true");
+  }
 }
